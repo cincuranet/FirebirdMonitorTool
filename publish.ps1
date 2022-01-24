77339463b534b543a1e7a9026db300440d49d755
Jiri Cincura <jiri@cincura.net>
2022-01-24 11:07:25 +0100

    Added "--self-contained" switch.

--- publish.ps1
+++ publish.ps1
@@ -5,8 +5,8 @@ if (Test-Path $outDir) {
 }
 mkdir $outDir | Out-Null
 
-dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -p:DebugType=embedded -r win-x64 .\src\FirebirdMonitorTool.ConsoleProfiler -o $outDir\win
-dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -p:DebugType=embedded -r linux-x64 .\src\FirebirdMonitorTool.ConsoleProfiler -o $outDir\linux
+dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -p:DebugType=embedded -r win-x64 --self-contained .\src\FirebirdMonitorTool.ConsoleProfiler -o $outDir\win
+dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -p:DebugType=embedded -r linux-x64 --self-contained .\src\FirebirdMonitorTool.ConsoleProfiler -o $outDir\linux
 
 Compress-Archive -Path $outDir\win\* -DestinationPath $outDir\FirebirdMonitorTool-win.zip -CompressionLevel Optimal
 Compress-Archive -Path $outDir\linux\* -DestinationPath $outDir\FirebirdMonitorTool-linux.zip -CompressionLevel Optimal
